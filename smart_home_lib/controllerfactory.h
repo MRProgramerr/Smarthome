9cc80a97b50b3400658802a6d0dae18ede0f8ef0
Harkaran Singh <sinhy040@mymail.unisa.edu.au>
2020-11-11 23:29:39 +1030

    Doxygen commented controller class and its functions

--- smart_home_lib/controllerfactory.h
+++ smart_home_lib/controllerfactory.h
@@ -3,11 +3,30 @@
 
 #include "deviceabstractfactory.h"
 
+/**
+ * @brief The ControllerFactory class
+ * The concrete factory class for creating
+ * controller objects according to the
+ * abstract factory pattern
+ */
+
 class ControllerFactory: public DeviceAbstractFactory
 {
 public:
+
+    /**
+     * @brief ControllerFactory
+     * The constructor for the controller factory class
+     * @param name
+     */
     ControllerFactory(QString name);
 
+    /**
+     * @brief createDevice
+     * The createDevice function creates the
+     * concrete controller object
+     * @return A concrete controller object
+     */
     Device *createDevice();
 
 private:
