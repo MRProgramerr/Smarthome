ddd8f20d655193070d1f65b2108da8858f41720a
Harkaran Singh <sinhy040@mymail.unisa.edu.au>
2020-11-11 23:40:53 +1030

    Doxygen commented mainMenu.cpp

--- smart_home_lib/mainmenu.h
+++ smart_home_lib/mainmenu.h
@@ -18,15 +18,37 @@ public:
    * @brief displayWelcome display an intial welcome message including the
    * student's name and game title.
    * @param author the name of the student
-   * @param title the name of the game
+   * @param title the name of the assignment
    */
   void displayWelcome(const QString &title, const QString &group, const QStringList &members) const;
 
+  /**
+   * @brief detailedUserInput
+   * The function to ask the user for specific details about
+   * a chosen device
+   * @param chosenDevice the device chosen by the user
+   *
+   */
   void detailedUserInput(QString chosenDevice);
 
+  /**
+   * @brief initialisingDevice
+   * The function to create a proxy using the
+   * abstract factory pattern
+   * @param chosenDevice
+   * @param deviceName
+   */
   void initialisingDevice(QString chosenDevice, QString deviceName);
 
+  /**
+   * @brief mainMenuLightSwitch
+   * The main menu for the lightSwitch concrete
+   * device. Uses proxies to talk to the
+   * real device for input and output
+   * @param lsp
+   */
   void mainMenuLightSwitch(QString lsp);
+
 
 
 public slots:
@@ -38,6 +60,7 @@ public slots:
   void run();
 
 private:
+
   QTextStream &_display;
   QTextStream &_input;
   int _userInput;
