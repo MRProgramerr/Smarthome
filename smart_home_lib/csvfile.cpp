a87b7e3a1fbc173194bf32a0f87a92e30099a18e
maiad002 <aldamaio1999@gmail.com>
2020-11-13 16:05:30 +1030

    Completed CSVfile implementation

--- smart_home_lib/csvfile.cpp
+++ smart_home_lib/csvfile.cpp
@@ -15,10 +15,9 @@ CsvFile::CsvFile(std::istream &input)
 
     bool endComma = false;
 
-  // initialises content of csv input into the 2d vector
-  // while there is input
-  while(input){
-
+    // initialises content of csv input into the 2d vector
+    // while there is input
+    while(input){
 
         // creates a string
         std::string str;
@@ -38,40 +37,186 @@ CsvFile::CsvFile(std::istream &input)
               endComma = true;
          }
 
+        // checks if the last input value is quoted
+        // last value is a special case since is ends with a newline and not a commma
+        bool lastQuoted = false;
+        if(str.at(length-1) == '"')
+        {
+            lastQuoted = true;
+        }
+
 
         // while there is a string stream
-        while(istr){
+        while(istr)
+        {
+
+            bool quoted = false;
+
+            int length = str2.length();
+
+            // new string to hold double quote string
+            std::string newstring = "";
+
+            // if the first character in the csv string are quotes
+            if(str2.at(0) == '"')
+            {
+                // loop through the string
+                for (int i = 0; i < length; i++)
+                {
+                    // whenever quotes are found
+                    if(str2.at(i) == '"')
+                    {
+                        // if these are internal quotes
+                        if(i != length-1 && str2.at(i+1) == '"')
+                        {
+                            // they are not the end quotes
+                            quoted = false;
+                            // adds internal quotes to string
+                            newstring += '"';
+                            // skips next quote
+                            i = i + 2;
+                        }
+                        // if these are the end quotes
+                        if(quoted == true)
+                        {
+                            // checks if there is an empty white space after the quote
+                            if(i != length -1 && str2.at(i+1) == ' ')
+                            {
+                                loadFail = true;
+                            }
+                            // left over characters (",) in the string are skipped
+                            if(i != length-1)
+                            {
+                                 i = i + 2;
+                            }
+                            // the left over characters in the string are skipped
+                            std::string str3 = str2;
+                            // second string is set to be empty
+                            str2 = "";
+                            // adds all content of the string with the quoted csv removed from str2
+                            for(; i< length; i++)
+                            {
+                                // adds everything from str3 but the quoted csv
+                                str2 += str3.at(i);
+                            }
+                            // str2 will now be sent through the process again with the next csv at the front
+                            break;
+                        }
+                    }
+
+                    // the starter double quotes have gone through
+                    quoted = true;
+                    // leaves out the double quote
+                    if(str2.at(i) != '"')
+                    {
+                        // checks for new line black slashes
+                        if(str2.at(i) == '\\' && str2.at(i -1) != '\\' && str2.at(i +1) != '\\' && str2.at(i +1) == 'n')
+                        {
+                            newstring += '\n';
+                            i = i + 2;
+                        }
+
+                        // adds the double quoted csv to a newstring with double quotes removed
+                         newstring += str2.at(i);
+                    }
+                }
+            }
+
+            // if the first character csv string are not quotes
+            else
+            {
+                // if the first character csv string is a white space
+                if(str2.at(0) == ' ')
+                {
+                    // if the character after it is a quote
+                    if(str2.at(1) == '"')
+                    {
+                        // there is a whitespace before a quote
+                        loadFail = true;
+                    }
+                }
+                // loop through string
+                 for (int i = 0; i < length; i++)
+                 {
+                     // until it finds the seperation comma
+                     if(str2.at(i) == ',' && endComma == false)
+                     {
+                         // new string to copy current string
+                         std::string str3 = str2;
+                         // current string is made empty
+                         str2 = "";
+                         // the comma is taken out
+                         i = i + 1;
+                        // removes the first csv value from the front of the string
+                         for(; i< length; i++)
+                         {
+                             str2 += str3.at(i);
+
+                         }
+                         break;
+                     }
+
+                 }
+
+            }
+
+
 
             // extracts string from input where a comma is found
           if(!getline(istr, str, ',')) break;
 
-            // push the string from the input without the comma into the vector
-          vec2.push_back(str);
+          if(quoted == false)
+          {
+              // push the string from the input without the comma into the vector
+              vec2.push_back(str);
+          }
+          if(quoted == true)
+          {
 
+              // the new line containing the double quoted text is pushed to the vector
+              // the str with the incorrect syntax gets skipped
+             vec2.push_back(newstring);
+
+              int strLength = str.length();
+              bool comma = false;
+
+              // If the value does not end with a '"' that mean there is a comma inside the quote that is part of the string
+              if(str.at(strLength-1) != '"')
+              {
+                   comma = true;
+              }
+
+              // If there was a comma as part of the string, the getline would've seperated it (e.g. "header,1" = becomes ("header) (1"))
+              // If it is the last value then it is seperated by newline instead of new comma, found by checking if the last value was quoted
+              if(comma == true || lastQuoted == true)
+              {
+                  // the string splits again, skipping over the seperated value
+                   if(!getline(istr, str, ',')) break;
+              }
+          }
         }
         // push the contents of vector 2 into the 2d vector
         vec.push_back(vec2);
     }
 
-  // checks if there are rows with extra columns
-  for (int i = 0; i < vec.size(); i++)
-  {
-      if(vec[i].size() > vec[0].size())
-      {
-         loadFail = true;
-      }
-  }
+    // checks if there are rows with extra columns
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if(vec[i].size() > vec[0].size())
+        {
+            loadFail = true;
+        }
+    }
 
-  // checks if there are rows with missing columns
-  for (int i = 0; i < vec.size(); i++)
-  {
-      // also checks if there was a comma at the end of the line, indicating empty field at end of line
-      if(vec[i].size() < vec[0].size() &&  endComma == false)
-      {
-         loadFail = true;
-      }
-  }
-
+    // checks if there are rows with missing columns
+    for (int i = 0; i < vec.size(); i++)
+    {
+        // also checks if there was a comma at the end of the line, indicating empty field at end of line
+        if(vec[i].size() < vec[0].size() &&  endComma == false)
+        {
+            loadFail = true;
+        }
+    }
 }
 
 int CsvFile::numberOfColumns() const
