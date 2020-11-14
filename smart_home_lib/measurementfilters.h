09fe73a20239f894d7fd455f9586134d7a3f45a5
Verma <versy016@mymail.unisa.edu.au>
2020-11-15 05:05:52 +1030

    added min conversion

--- smart_home_lib/measurementfilters.h
+++ smart_home_lib/measurementfilters.h
@@ -9,21 +9,42 @@ class MeasurementFilters : public MeasurementTemplate<T>
 public:
     MeasurementFilters(std::string DeviceName, std::string MeasurementType,std::string FilterType) : MeasurementTemplate<T>(DeviceName,MeasurementType, FilterType){}
     void setConfiguration(T conifguration){
-        Configuration = conifguration;
+        configuireval = conifguration;
     }
     T getConfiguration(){
-        return Configuration;
+        return configuireval;
     }
     ~MeasurementFilters(){};
 
     T MinConversion(){
-        if(!isvalid(Configuration)){
+        if(!isvalid(configuireval)){
                 return "*invalid*";
     }
-        T lastVal = MeasurementTemplate<T>().value();
+        else if(getinclusive()){
 
+            if(MeasurementTemplate<T>().value() >= configuireval)
+                return configuireval;
+            else
+                return MeasurementTemplate<T>().value();
+        }
+
+        else if(!getinclusive()){
+
+            if(MeasurementTemplate<T>().value() > configuireval)
+                return configuireval;
+            else
+                return MeasurementTemplate<T>().value();
+        }
     }
 
+    T TempConversion(){
+        if(MeasurementTemplate<T>::unitOfMeasure() == "C" || MeasurementTemplate<T>::unitOfMeasure() == "C")
+          return  (MeasurementTemplate<T>::value() * (9.0/5.0f))+32;
+        else{
+            return (MeasurementTemplate<T>::value()- 32) *(5.0/9.0);
+        }
+
+}
     bool isvalid(QVariant value){
 
         if(value.type() == QVariant::Double || value.type() == QVariant::Time || value.type() == QVariant::Int){
@@ -33,9 +54,16 @@ public:
             return false;
     }
 
-private:
-    T Configuration ;
+    void setinclusive(bool inc){
+        Inc = inc;
+    }
+    bool getinclusive(){
+        return Inc;
+    }
 
+private:
+    T configuireval ;
+    bool Inc = true;
 };
 
 
